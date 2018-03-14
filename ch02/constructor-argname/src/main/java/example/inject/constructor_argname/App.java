04b4d14b541d70549a0a1ed585bc0338814958fb
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-03-14 20:04:18 +0900

    2-1-3: constructor injection using name attribute of xml-config

---
+++ ch02/constructor-argname/src/main/java/example/inject/constructor_argname/App.java
@@ -0,0 +1,23 @@
+package example.inject.constructor_argname;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(AbstractApplicationContext context 
+    			= new ClassPathXmlApplicationContext("applicationContext.xml")) {
+	    	UserService userService = context.getBean(UserService.class);
+	    	User user = new User("sasaki");
+	    	userService.register(user, "seiji");
+	    	System.out.println("username: " + user.getUsername());
+	    	System.out.println("password: " + user.getPassword());
+    	}
+    }
+}
---
+++ ch02/constructor-argname/src/main/java/example/inject/constructor_argname/UserService.java
@@ -0,0 +1,5 @@
+package example.inject.constructor_argname;
+
+public interface UserService {
+	void register(User user, String rawPassword);
+}
