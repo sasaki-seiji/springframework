c1f4e75a51fcb0024b8b1c6ade2213d3ccb0baa2
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-03-21 13:03:34 +0900

    delete @Component from DomainService implementation classes

--- ch02/scanfilter-assignable-java/src/main/java/example/componentscan/scanfilter_assignable_java/DomainServiceImpl.java
+++ ch02/scanfilter-assignable-java/src/main/java/example/componentscan/scanfilter_assignable_java/DomainServiceImpl.java
@@ -2,7 +2,7 @@ package example.componentscan.scanfilter_assignable_java;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class DomainServiceImpl implements DomainService {
 
 }
--- ch02/scanfilter-assignable-java/src/main/java/example/componentscan/scanfilter_assignable_java/SomeDomainService.java
+++ ch02/scanfilter-assignable-java/src/main/java/example/componentscan/scanfilter_assignable_java/SomeDomainService.java
@@ -2,7 +2,7 @@ package example.componentscan.scanfilter_assignable_java;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class SomeDomainService implements DomainService {
 
 }
