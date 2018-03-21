8413721fce9e037ca0b097a76ed295e6328a4fdc
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-03-21 14:14:52 +0900

    2.1.6: @ComponentScan.Filter assignable type : xml config

---
+++ ch02/scanfilter-assignable-xml/src/main/java/example/componentscan/scanfilter_assignable_xml/AnotherComponent.java
@@ -0,0 +1,8 @@
+package example.componentscan.scanfilter_assignable_xml;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnotherComponent {
+
+}
---
+++ ch02/scanfilter-assignable-xml/src/main/java/example/componentscan/scanfilter_assignable_xml/DomainService.java
@@ -0,0 +1,5 @@
+package example.componentscan.scanfilter_assignable_xml;
+
+public interface DomainService {
+
+}
