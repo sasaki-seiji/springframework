295c3fe9107767d5bc203a76957cccbe9b6a67fe
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-03-24 15:59:59 +0900

    resolve warning

--- ch02/scope-prototype-xml/src/main/java/example/scope/scope_prototype_xml/App.java
+++ ch02/scope-prototype-xml/src/main/java/example/scope/scope_prototype_xml/App.java
@@ -1,6 +1,5 @@
 package example.scope.scope_prototype_xml;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
