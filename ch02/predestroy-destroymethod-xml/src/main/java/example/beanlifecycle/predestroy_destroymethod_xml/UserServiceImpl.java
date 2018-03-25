ca285d109a7287919d72e2a6fc40da6836754749
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-03-25 18:49:53 +0900

    2.1.8: <bean destroy-method="XXX">

---
+++ ch02/predestroy-destroymethod-xml/src/main/java/example/beanlifecycle/predestroy_destroymethod_xml/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package example.beanlifecycle.predestroy_destroymethod_xml;
+
+
+public class UserServiceImpl implements UserService {
+	private final UserRepository userRepository;
+
+	public UserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	void clearCache() {
+		System.out.println("clearCache: " + this);
+	}
+	
+	@Override
+	public void register(User user, String rawPassword) {
+		if (this.userRepository.countByUsername(user.getUsername()) > 0) {
+			throw new UserAlreadyRegisteredException();
+		}
+		
+		user.setPassword(rawPassword);
+		this.userRepository.save(user);
+	}
+}
