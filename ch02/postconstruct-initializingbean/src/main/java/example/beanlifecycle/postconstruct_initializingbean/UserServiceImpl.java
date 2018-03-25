5b4f280913d3afe0c06391f668657e9362f137eb
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-03-25 18:18:42 +0900

    resolve warning

--- ch02/postconstruct-initializingbean/src/main/java/example/beanlifecycle/postconstruct_initializingbean/UserServiceImpl.java
+++ ch02/postconstruct-initializingbean/src/main/java/example/beanlifecycle/postconstruct_initializingbean/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package example.beanlifecycle.postconstruct_initializingbean;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
