03302d8179459f272f9abd3fc7c00fd5ca0df05e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-07 17:41:17 +0900

    2.4.1: place-holder in XML-config

---
+++ ch02/2-4/xmlconfig-placeholder/src/main/java/example/property/xmlconfig_placeholder/BasicDataSource.java
@@ -0,0 +1,49 @@
+package example.property.xmlconfig_placeholder;
+
+public class BasicDataSource implements DataSource {
+
+	private String className;
+	private String url;
+	private String userName;
+	private String password;
+	private boolean autoCommit;
+	
+	@Override
+	public void setDriverClassName(String className) {
+		this.className = className;
+	}
+
+	@Override
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public void setUsername(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public void setDefaultAutoCommit(boolean autoCommit) {
+		this.autoCommit = autoCommit;
+	}
+
+	@Override
+	public void close() {
+		System.out.println("BasicDataSource: closed");
+	}
+
+	@Override
+	public String toString() {
+		return "driverClass: " + className + "\n"
+				+ "url: " + url + "\n"
+				+ "userName: " + userName + "\n"
+				+ "password: " + password + "\n"
+				+ "autoCommit: " + autoCommit + "\n";
+	}
+}
