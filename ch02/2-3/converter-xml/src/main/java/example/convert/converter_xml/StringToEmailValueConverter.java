e3b0e94a34b445f65bccbae866f277e83cd576df
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-08 17:44:22 +0900

    2.3.6: Converter<String,EmailValue> (XML-config)

---
+++ ch02/2-3/converter-xml/src/main/java/example/convert/converter_xml/StringToEmailValueConverter.java
@@ -0,0 +1,13 @@
+package example.convert.converter_xml;
+
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToEmailValueConverter implements Converter<String, EmailValue> {
+	@Override
+	public EmailValue convert(String source) {
+		EmailValue email = new EmailValue();
+		email.setValue(source);
+		return email;
+	}
+
+}
