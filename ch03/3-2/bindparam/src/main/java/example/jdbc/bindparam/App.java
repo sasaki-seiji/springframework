a3049826c45939700d059bd797ae4af862019037
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-15 15:56:15 +0900

    3.2.2: SQL using bind parameter

---
+++ ch03/3-2/bindparam/src/main/java/example/jdbc/bindparam/App.java
@@ -0,0 +1,21 @@
+package example.jdbc.bindparam;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+    			= new ClassPathXmlApplicationContext("JdbcTemplateConfig.xml")) {
+    		JdbcRoomDao dao = context.getBean("jdbcRoomDao", JdbcRoomDao.class);
+    		String roomName = dao.findRoomNameById("X9999");
+    		System.out.println("roomName: " + roomName);
+    	}
+    }
+}
---
+++ ch03/3-2/bindparam/src/main/java/example/jdbc/bindparam/JdbcRoomDao.java
@@ -0,0 +1,22 @@
+package example.jdbc.bindparam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JdbcRoomDao {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	public int findMaxCapacity() {
+		String sql = "SELECT MAX(capacity) FROM room";
+		return jdbcTemplate.queryForObject(sql, Integer.class);
+	}
+	
+	public String findRoomNameById(String roomId) {
+		String sql = "SELECT room_name FROM room WHERE room_id = ?";
+		return jdbcTemplate.queryForObject(sql, String.class, roomId);
+	}
+}
