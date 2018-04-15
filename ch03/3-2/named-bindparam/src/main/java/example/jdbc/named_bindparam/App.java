fcf79002d3c0d482b4b8f9c181a11ceef25b9e16
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-15 16:42:28 +0900

    3.2.2: SQL using named bind parameter

---
+++ ch03/3-2/named-bindparam/src/main/java/example/jdbc/named_bindparam/App.java
@@ -0,0 +1,22 @@
+package example.jdbc.named_bindparam;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+    			= new ClassPathXmlApplicationContext("JdbcTemplateConfig.xml")) {
+    		JdbcRoomNamedDao dao = context.getBean("jdbcRoomNamedDao", JdbcRoomNamedDao.class);
+    		String roomName = dao.findRoomNameById("X9999");
+    		System.out.println("roomName: " + roomName);
+    	}
+    }
+}
