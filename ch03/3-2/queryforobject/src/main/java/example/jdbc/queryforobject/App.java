6de59ab9e251d2a5db69d93c526d117e2e9815e2
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-15 15:25:55 +0900

    3.2.2: JdbcTemplate.queryForObject

---
+++ ch03/3-2/queryforobject/src/main/java/example/jdbc/queryforobject/App.java
@@ -0,0 +1,21 @@
+package example.jdbc.queryforobject;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+    			= new ClassPathXmlApplicationContext("JdbcTemplateConfig.xml")) {
+    		JdbcRoomDao dao = context.getBean("jdbcRoomDao", JdbcRoomDao.class);
+    		int maxCapacity = dao.findMaxCapacity();
+    		System.out.println("maxCapacity: " + maxCapacity);
+    	}
+    }
+}
