ae782cdc9ccc5086bc4dc692330b652e023d40bc
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-15 19:53:55 +0900

    3.2.2: JdbcTemplate.queryForMap()

---
+++ ch03/3-2/queryformap/src/main/resources/insert-init-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO room VALUES ('A001', '幹部用会議室', 10);
+INSERT INTO room VALUES ('C001001', 'セミナールーム', 30);
+INSERT INTO room VALUES ('X9999', 'カンファレンスルーム', 100);
+
+INSERT INTO equipment VALUES ('10-1', 'A001', 'テレビ会議システム', 1, NULL);
+INSERT INTO equipment VALUES ('20-1', 'A001', 'プロジェクタ', 1, '部屋備え付けです');
+INSERT INTO equipment VALUES ('40-500', 'C001001', 'シンクライアント', 10, NULL);
+INSERT INTO equipment VALUES ('20-2', 'C001001', 'プロジェクタ', 5, '移動可能です');
+INSERT INTO equipment VALUES ('30-1', 'C001001', 'ホワイトボード', 6, '移動可能です');
