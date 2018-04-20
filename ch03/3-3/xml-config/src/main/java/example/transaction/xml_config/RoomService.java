3592f3ee8f7d3e53c51c7675dcd5fa71ad64d583
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-21 08:19:04 +0900

    3.3.2: declarative transaction using XML-config

---
+++ ch03/3-3/xml-config/src/main/java/example/transaction/xml_config/RoomService.java
@@ -0,0 +1,6 @@
+package example.transaction.xml_config;
+
+public interface RoomService {
+	Room getRoom(String roomId);
+	void insertRoom(Room room);
+}
---
+++ ch03/3-3/xml-config/src/main/java/example/transaction/xml_config/RoomServiceImpl.java
@@ -0,0 +1,27 @@
+package example.transaction.xml_config;
+
+import java.util.List;
+
+
+public class RoomServiceImpl implements RoomService {
+
+	private JdbcRoomDao	jdbcRoomDao;
+	public void setJdbcRoomDao(JdbcRoomDao jdbcRoomDao) {
+		this.jdbcRoomDao = jdbcRoomDao;
+	}
+	
+	@Override
+	public Room getRoom(String roomId) {
+		return jdbcRoomDao.getRoomWithEquipmentById(roomId);
+	}
+
+	@Override
+	public void insertRoom(Room room) {
+		jdbcRoomDao.insertRoom(room);
+		List<Equipment> equipmentList = room.getEquipmentList();
+		for(Equipment item: equipmentList) {
+			jdbcRoomDao.insertEquipment(item);
+		}
+	}
+
+}
