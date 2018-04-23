fb6eedafa703c2229d505062707f8b92deea7218
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-24 00:53:16 +0900

    add user_table SQL for PostgreSQL

---
+++ sql/insert-user.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_table VALUES (1, 'mkyong', 'mkyong@gmail.com');
+INSERT INTO user_table VALUES (2, 'alex', 'alex@yahoo.com');
+INSERT INTO user_table VALUES (3, 'joel', 'joel@gmail.com');
---
+++ sql/schema-user.sql
@@ -0,0 +1,6 @@
+DROP TABLE user_table;
+CREATE TABLE user_table (
+  user_id   VARCHAR(10) PRIMARY KEY,
+  user_name VARCHAR(30),
+  email  VARCHAR(50)
+);
