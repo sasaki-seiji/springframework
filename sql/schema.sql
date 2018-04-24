d3bb82aa0a46994cb8ecf8cf5f922b31aea0d89b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-24 20:40:29 +0900

    add SQL for room-table (postgres)

---
+++ sql/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE equipment;
+DROP TABLE room;
+CREATE TABLE room (
+  room_id       VARCHAR(10) NOT NULL PRIMARY KEY,
+  room_name 	VARCHAR(30) NOT NULL,
+  capacity		INT NOT NULL
+);
+
+CREATE TABLE equipment (
+  equipment_id		VARCHAR(10) NOT NULL PRIMARY KEY,
+  room_id			VARCHAR(10) NOT NULL,
+  equipment_name 	VARCHAR(30) NOT NULL,
+  equipment_count	INT NOT NULL,
+  equipment_remarks	VARCHAR(100),
+  FOREIGN KEY (room_id) REFERENCES room(room_id)
+);
