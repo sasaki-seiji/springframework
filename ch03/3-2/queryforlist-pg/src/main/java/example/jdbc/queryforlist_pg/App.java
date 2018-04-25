21e6c5a30b557044942ca04cd55d1f9d70b9cc2c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-25 20:51:47 +0900

    3.2.2: JdbcTemplate.queryForList() (PostgreSQL)

---
+++ ch03/3-2/queryforlist-pg/src/main/java/example/jdbc/queryforlist_pg/App.java
@@ -0,0 +1,23 @@
+package example.jdbc.queryforlist_pg;
+
+import java.util.List;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+    			= new ClassPathXmlApplicationContext("JdbcTemplateConfig.xml")) {
+    		JdbcRoomDao dao = context.getBean("jdbcRoomDao", JdbcRoomDao.class);
+    		List<Room> rooms = dao.getAllRoom();
+    		System.out.println("rooms: " + rooms);
+    	}
+    }
+}
---
+++ ch03/3-2/queryforlist-pg/src/main/java/example/jdbc/queryforlist_pg/JdbcRoomDao.java
@@ -0,0 +1,50 @@
+package example.jdbc.queryforlist_pg;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JdbcRoomDao {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	public int findMaxCapacity() {
+		String sql = "SELECT MAX(capacity) FROM room";
+		return jdbcTemplate.queryForObject(sql, Integer.class);
+	}
+	
+	public String findRoomNameById(String roomId) {
+		String sql = "SELECT room_name FROM room WHERE room_id = ?";
+		return jdbcTemplate.queryForObject(sql, String.class, roomId);
+	}
+	
+	public Room getRoomById(String roomId) {
+		String sql = "SELECT room_id, room_name, capacity FROM room WHERE room_id = ?";
+		Map<String, Object> result = jdbcTemplate.queryForMap(sql, roomId);
+		Room room = new Room();
+		room.setRoomId((String)result.get("room_id"));
+		room.setRoomName((String)result.get("room_name"));
+		room.setCapacity((Integer)result.get("capacity"));
+		return room;
+	}
+	
+	public List<Room> getAllRoom() {
+		String sql = "SELECT room_id, room_name, capacity FROM room";
+		List<Map<String, Object>> resultList = jdbcTemplate.queryForList(sql);
+		List<Room> roomList = new ArrayList<Room>();
+		for(Map<String, Object> result: resultList) {
+			Room room = new Room();
+			room.setRoomId((String)result.get("room_id"));
+			room.setRoomName((String)result.get("room_name"));
+			room.setCapacity((Integer)result.get("capacity"));
+			roomList.add(room);
+		}
+		return roomList;
+	}
+}
