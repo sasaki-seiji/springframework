13efe24433d6410b91530fc0683085c54f7506a7
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-25 20:40:55 +0900

    3.2.2: JdbcTemplate.queryForMap() (PostgreSQL)

---
+++ ch03/3-2/queryformap-pg/src/main/java/example/jdbc/queryformap_pg/App.java
@@ -0,0 +1,21 @@
+package example.jdbc.queryformap_pg;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+    			= new ClassPathXmlApplicationContext("JdbcTemplateConfig.xml")) {
+    		JdbcRoomDao dao = context.getBean("jdbcRoomDao", JdbcRoomDao.class);
+    		Room room = dao.getRoomById("X9999");
+    		System.out.println("room: " + room);
+    	}
+    }
+}
