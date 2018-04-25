8ab295c307d23853be26e6811202321f8b6a6d45
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-25 19:32:05 +0900

    3.2.2: MapSeqlParameterSource (PostgreSQL)

---
+++ ch03/3-2/mapsqlparametersource-pg/src/main/java/example/jdbc/mapsqlparametersource_pg/App.java
@@ -0,0 +1,28 @@
+package example.jdbc.mapsqlparametersource_pg;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+    			= new ClassPathXmlApplicationContext("JdbcTemplateConfig.xml")) {
+    		NamedParameterJdbcTemplate template = context.getBean("namedParameterJdbcTemplate", NamedParameterJdbcTemplate.class);
+    		MapSqlParameterSource map = new MapSqlParameterSource()
+    				.addValue("roomId", "A001")
+    				.addValue("roomName", "幹部用会議室")
+    				.addValue("capacity", 10);
+    		String sql = "SELECT COUNT(*) FROM room WHERE room_id = :roomId AND room_name = :roomName AND capacity = :capacity";
+    		int count = template.queryForObject(sql,  map,  Integer.class);
+    		System.out.println("count: " + count);
+    	}
+    }
+}
