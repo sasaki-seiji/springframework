42a37c35d225a9824d5764aff9e6f129ad420d98
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-27 19:50:01 +0900

    3.2.3: ResultSetExtractor (PostgreSQL)

---
+++ ch03/3-2/resultsetextractor-pg/src/main/java/example/jdbc/resultsetextractor_pg/Room.java
@@ -0,0 +1,28 @@
+package example.jdbc.resultsetextractor_pg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Room {
+	private String roomId;
+	private String roomName;
+	private Integer capacity;
+	private List<Equipment> equipmentList = new ArrayList<Equipment>(); 
+	
+	public Room(String roomId, String roomName, Integer capacity) {
+		this.roomId = roomId; this.roomName = roomName; this.capacity = capacity;
+	}
+	public Room() {}
+	
+	public String getRoomId() { return this.roomId; }
+	public void setRoomId(String roomId) { this.roomId = roomId; }
+	public String getRoomName() { return this.roomName; }
+	public void setRoomName(String roomName) { this.roomName = roomName; }
+	public Integer getCapacity() { return this.capacity; }
+	public void setCapacity(Integer capacity) { this.capacity = capacity; }
+	public List<Equipment> getEquipmentList() { return this.equipmentList; }
+
+	public String toString() {
+		return "{ roomId: " + roomId + ", roomName: " + roomName + ", capacity: " + capacity + ", equipmentList: " + equipmentList + " }";
+	}
+}
---
+++ ch03/3-2/resultsetextractor-pg/src/main/java/example/jdbc/resultsetextractor_pg/RoomRowMapper.java
@@ -0,0 +1,19 @@
+package example.jdbc.resultsetextractor_pg;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class RoomRowMapper implements RowMapper<Room> {
+	
+	@Override
+	public Room mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Room room = new Room();
+		room.setRoomId(rs.getString("room_id"));
+		room.setRoomName(rs.getString("room_name"));
+		room.setCapacity(rs.getInt("capacity"));
+		return room;
+	}
+
+}
