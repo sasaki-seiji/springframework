4a64104272644e7157b4b2debca4b598b71a52da
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-04-28 06:25:41 +0900

    3.3.2: @Transactional (PostgreSQL)

---
+++ ch03/3-3/transactional-pg/src/main/java/example/transaction/transactional_pg/Equipment.java
@@ -0,0 +1,26 @@
+package example.transaction.transactional_pg;
+
+public class Equipment {
+	private String equipmentId;
+	private String roomId;
+	private String equipmentName;
+	private Integer equipmentCount;
+	private String equipmentRemarks;
+	
+	String getEquipmentId() { return this.equipmentId; }
+	void setEquipmentId(String equipmentId) { this.equipmentId = equipmentId; }
+	String getRoomId() { return this.roomId; }
+	void setRoomId(String roomId) { this.roomId = roomId; }
+	String getEquipmentName() { return this.equipmentName; }
+	void setEquipmentName(String equipmentName) { this.equipmentName = equipmentName; }
+	Integer getEquipmentCount() { return this.equipmentCount; }
+	void setEquipmentCount(Integer equipmentCount) { this.equipmentCount = equipmentCount; }
+	String getEquipmentRemarks() { return this.equipmentRemarks; }
+	void setEquipmentRemarks(String equipmentRemarks) { this.equipmentRemarks = equipmentRemarks; }
+	
+	public String toString() {
+		return "{ equipmentId: " + equipmentId + ", roomId: " + roomId
+				+ ", equipmentName: " + equipmentName + ", equipmentCount: " + equipmentCount
+				+ ", equipmentRemaks: " + equipmentRemarks + " }";
+	}
+}
