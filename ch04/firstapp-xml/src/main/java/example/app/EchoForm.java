99fec5a6dd95244bf3c90974564281b720b81ae4
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-05-02 12:25:09 +0900

    bug fix

--- ch04/firstapp-xml/src/main/java/example/app/EchoForm.java
+++ ch04/firstapp-xml/src/main/java/example/app/EchoForm.java
@@ -1,4 +1,4 @@
-package example_xml.app;
+package example.app;
 
 import java.io.Serializable;
 
--- ch04/firstapp-xml/src/main/java/example/app/WelcomeController.java
+++ ch04/firstapp-xml/src/main/java/example/app/WelcomeController.java
@@ -1,4 +1,4 @@
-package example_xml.app;
+package example.app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
