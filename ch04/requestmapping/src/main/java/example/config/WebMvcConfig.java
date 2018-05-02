86f71fd28af914eb6db1e06e212bd610c63b17b8
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-05-02 15:23:28 +0900

    resolve deprecated WebMvcConfigurerAdapter

--- ch04/requestmapping/src/main/java/example/config/WebMvcConfig.java
+++ ch04/requestmapping/src/main/java/example/config/WebMvcConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("example.app")
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
