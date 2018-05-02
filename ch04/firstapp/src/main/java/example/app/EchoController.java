a52b3d1d83c91813f391a20d96b72a451b4db8fe
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-05-02 12:29:27 +0900

    bug fix

--- ch04/firstapp/src/main/java/example/app/EchoController.java
+++ ch04/firstapp/src/main/java/example/app/EchoController.java
@@ -8,7 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Controller("echo")
 public class EchoController {
 
 	@RequestMapping(method=RequestMethod.GET)
