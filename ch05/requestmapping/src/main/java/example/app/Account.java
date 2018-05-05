4983198ffc6cd9da3d85e3a0450cab6e5971371e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-05-06 07:30:04 +0900

    add Account::Account(...)

--- ch05/requestmapping/src/main/java/example/app/Account.java
+++ ch05/requestmapping/src/main/java/example/app/Account.java
@@ -5,6 +5,13 @@ public class Account {
 	private String name;
 	private String email;
 	
+	public Account() { }
+	public Account(String id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	
 	public String getId() { return id; }
 	public void setId(String id) { this.id = id; }
 	public String getName() { return name; }
