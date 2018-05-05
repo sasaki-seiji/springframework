d5ef04e3be047c51d1ab5fe5ffe365db1e4d7965
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-05-05 17:08:08 +0900

    5.4.3:@RequestMapping using HTTP method

--- ch05/requestmapping/src/main/java/example/app/AccountController.java
+++ ch05/requestmapping/src/main/java/example/app/AccountController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("accounts")
@@ -19,7 +20,7 @@ public class AccountController {
 		return "email";
 	}
 	
-	@RequestMapping("{accountId}")
+	@RequestMapping(path = "{accountId}", method = RequestMethod.GET)
 	public String showAccount(@PathVariable String accountId, Model model) {
 		Account account = accountService.getAccount(accountId);
 		model.addAttribute(account);
