f80a7887c4f0bd1f5c5c0d03c25e533ba939cbc1
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-05-10 20:12:49 +0900

    move datetimeformat project to ch05/5-5

---
+++ ch05/5-5/datetimeformat/src/main/java/example/app/MenuController.java
@@ -0,0 +1,15 @@
+package example.app;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MenuController {
+
+	@RequestMapping("/")
+	public String menu() {
+		return "menu"; 
+	}
+	
+}
---
+++ ch05/5-5/datetimeformat/src/main/webapp/WEB-INF/requestparam/result.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+	<h2>変換結果</h2>
+	<div>targetDate.toString():</div>
+	<div>${result}</div>
+	<ul>
+		<li><a href="<c:url value='/' />">メニューに戻る</a></li>
+	</ul>
+</body>
+</html>
