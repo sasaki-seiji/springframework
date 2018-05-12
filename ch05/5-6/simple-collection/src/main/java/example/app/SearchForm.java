80265ff950adf460d1e8e1e3938bdf5de607cbed
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-05-12 09:00:42 +0900

    move project to collect folder

---
+++ ch05/5-6/simple-collection/src/main/java/example/app/SearchForm.java
@@ -0,0 +1,11 @@
+package example.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SearchForm {
+	private List<String> roles = new ArrayList<String>();
+	
+	public List<String> getRoles() { return roles; }
+}
---
+++ ch05/5-6/simple-collection/src/main/webapp/WEB-INF/menu.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>メニュー画面</h2>
+<ul>
+	<li><a href="<c:url value='/accounts/search?form=' />">検索</a></li>
+</ul>
+</body>
+</html>
