ad68b0c29d270e99b993285cace7a4d5e6882e70
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-07-07 15:40:40 +0900

    6.5.2: create subclass of ResponseEntityExceptionHandler

---
+++ ch06/6-5/responseentityexceptionhandler/src/main/java/example/app/ApiExceptionHandler.java
@@ -0,0 +1,9 @@
+package example.app;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
