0634e3f4698c8a6020723656086811b3a2cb3435
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-07-16 18:30:50 +0900

    6.5.5 add getBookSlow() handler for timeout check:6.6.5

--- ch06/6-5/handle-servleterror/src/main/java/example/app/BooksRestController.java
+++ ch06/6-5/handle-servleterror/src/main/java/example/app/BooksRestController.java
@@ -64,4 +64,27 @@ public class BooksRestController {
 		throw new RuntimeException("artificial exception");
 	}
 	
+	// 2018.07.16 add for timeout test
+	@RequestMapping(path = "slow/{bookId}", method = RequestMethod.GET)
+	public BookResource getBookSlow(@PathVariable String bookId) {
+
+		// delay 5000ms
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		Book book = bookService.find(bookId);
+		if (book == null) {
+			throw new BookResourceNotFoundException(bookId);
+		}
+		
+		BookResource resource = new BookResource();
+		resource.setBookId(book.getBookId());
+		resource.setName(book.getName());
+		resource.setPublishedDate(book.getPublishedDate());
+		
+		return resource;
+	}
 }
