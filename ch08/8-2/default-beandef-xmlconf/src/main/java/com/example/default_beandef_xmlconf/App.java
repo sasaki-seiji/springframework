0706e12c6a5a26e956f951e046fbb4794bedf388
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-08-19 17:47:24 +0900

    8.2.4: default Bean definition (XML-config)

---
+++ ch08/8-2/default-beandef-xmlconf/src/main/java/com/example/default_beandef_xmlconf/App.java
@@ -0,0 +1,13 @@
+package com.example.default_beandef_xmlconf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
