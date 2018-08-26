1570f5511bad47c30599799c4810dfbdab9fbfbd
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-08-26 19:21:38 +0900

    3.2.2: queryformap: maintain pom.xml

--- ch03/3-2/queryformap/src/main/java/example/jdbc/queryformap/App.java
+++ ch03/3-2/queryformap/src/main/java/example/jdbc/queryformap/App.java
@@ -4,10 +4,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
