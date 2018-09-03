3fa6e17efd8f75eb5972f2ac31f70dbe8b02dc88
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-09-03 20:43:20 +0900

    3.4.2: handle-exception: maintain pom.xml

--- ch03/3-4/handle-exception/src/main/java/example/dataaccessexception/handle_exception/App.java
+++ ch03/3-4/handle-exception/src/main/java/example/dataaccessexception/handle_exception/App.java
@@ -3,10 +3,6 @@ package example.dataaccessexception.handle_exception;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
