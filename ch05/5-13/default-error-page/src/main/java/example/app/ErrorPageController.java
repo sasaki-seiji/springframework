1ae5f08bca2173014a804ded068933922f2e7dd7
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-09-15 18:50:18 +0900

    5.13.3: default-error-page

---
+++ ch05/5-13/default-error-page/src/main/java/example/app/ErrorPageController.java
@@ -0,0 +1,27 @@
+package example.app;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ErrorPageController {
+
+	@RequestMapping(path="/", method=RequestMethod.GET)
+	public String form() {
+		return "form";
+	}
+
+	@RequestMapping(path="/exception", method=RequestMethod.GET)
+	public String exception() throws Exception {
+		throw new Exception("throw Exception");
+	}
+
+	@RequestMapping(path="/ioexception", method=RequestMethod.GET)
+	public String ioexception() throws Exception {
+		throw new IOException("throw Exception");
+	}
+	
+}
