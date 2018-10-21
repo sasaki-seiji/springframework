fecb70423fe9658034e86a6d46aef78ff8e7c3f8
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-10-21 19:52:23 +0900

    9.4.4: failure-url

---
+++ ch09/9-4/failure-url/src/main/java/example/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package example.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+            http.formLogin()
+            		.loginPage("/login")
+            		.permitAll();
+            http.formLogin()
+            		.failureUrl("/loginFailure")
+            		.permitAll();
+            http.authorizeRequests()
+            		.anyRequest().authenticated();
+    }
+}
---
+++ ch09/9-4/failure-url/src/main/webapp/views/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Welcome to failure-url project</h2>
+</body>
+</html>
