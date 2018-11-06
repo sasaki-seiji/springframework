2f5158b76ba316ae8d01bc208259e8b82a78ed85
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-11-06 20:20:40 +0900

    9.4.6: bcryptpasswordencoder-xml

---
+++ ch09/9-4/bcryptpasswordencoder-xml/src/main/resources/insert-init-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO account VALUES ('guest', '$2a$10$IGr6WwwMYT9yjGMHZygOYeCF2POdgqrYV2YlY2n0UhLx7M6lTqigm', TRUE, FALSE);
+INSERT INTO account VALUES ('admin', '$2a$10$uyXIQe9yhzo1kt08DcXRfeIqax43O2cD6puA5626fnslCVFcjEo.S', TRUE, TRUE);
+INSERT INTO account VALUES ('disabled', '$2a$10$za2id1t3HP2/hH2LRdsDNObDKilwF0AW77OuG7By7MrmLUutn54Va', FALSE, FALSE);
