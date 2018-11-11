8ffd11aa72f3138bd81db8bafd7920813187aed0
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-11-11 10:36:02 +0900

    9.4.2: form-login: remove userDetailsServiceBean()

--- ch09/9-4/form-login/src/main/java/example/config/WebSecurityConfig.java
+++ ch09/9-4/form-login/src/main/java/example/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,31 +21,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 	
 	// 2018.11.06 add: 
-	// see https://docs.spring.io/spring-security/site/docs/4.2.5.RELEASE/apidocs/org/springframework/security/config/annotation/web/configuration/WebSecurityConfigurerAdapter.html#configure-org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder-
+	// see https://docs.spring.io/spring-security/site/docs/5.1.1.RELEASE/api/https://docs.spring.io/spring-security/site/docs/5.1.1.RELEASE/api/
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 		// enable in memory based authentication with a user named
 		// "guest" and "admin"
 		.inMemoryAuthentication()
-		.passwordEncoder(passwordEncoder())
 		.withUser("guest").password("guest").roles("USER").and()
 		.withUser("admin").password("admin").roles("USER", "ADMIN");
 	}
 
-	// 2018.11.06 add: 
-	// see https://docs.spring.io/spring-security/site/docs/4.2.5.RELEASE/apidocs/org/springframework/security/config/annotation/web/configuration/WebSecurityConfigurerAdapter.html#configure-org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder-
-	// Expose the UserDetailsService as a Bean
-	@Bean
-	@Override
-	public UserDetailsService userDetailsServiceBean() throws Exception {
-		return super.userDetailsServiceBean();
-	}
-
-	// 2018.11.06 add
+	// 2018.11.11 add: needed for spring security 5
+	// see https://www.harinathk.com/spring/no-passwordencoder-mapped-id-null/
+	@SuppressWarnings("deprecation")
 	@Bean
 	PasswordEncoder passwordEncoder() {
-		//return new BCryptPasswordEncoder();
 		return NoOpPasswordEncoder.getInstance(); // tempolary use
 	}
 
