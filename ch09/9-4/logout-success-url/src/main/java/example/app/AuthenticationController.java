77ed9111115eed0b9b2930790ca7956f103d8c92
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-11-13 20:25:26 +0900

    9.4.9: logout-success-url

---
+++ ch09/9-4/logout-success-url/src/main/java/example/app/AuthenticationController.java
@@ -0,0 +1,19 @@
+package example.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AuthenticationController {
+
+	@RequestMapping(path = "/login", method = RequestMethod.GET)
+	public String viewLoginForm() {
+		return "loginForm";
+	}
+
+	@RequestMapping(path = "/logoutSuccess", method = RequestMethod.GET)
+	public String viewLogoutSucess() {
+		return "logoutSuccess";
+	}
+}
---
+++ ch09/9-4/logout-success-url/src/main/webapp/views/logoutSuccess.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>ログアウト成功</h2>
+<a href="<c:url value='/login'/>">ログイン画面</a>
+</body>
+</html>
