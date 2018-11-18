0c9dd745cb3006b21d5774fe14742a3d400abdbe
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-11-18 20:34:32 +0900

    9.5.5: preauthorize: try service interface

--- ch09/9-5/preauthorize/src/main/java/example/app/AccountService.java
+++ ch09/9-5/preauthorize/src/main/java/example/app/AccountService.java
@@ -2,26 +2,12 @@ package example.app;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Transactional
-public class AccountService {
+public interface AccountService {
 
-	@Autowired
-	AccountRepository accountRepository;
+	List<String> getAllUsernames() ;
 
-	@Transactional(readOnly = true)
-	public List<String> getAllUsernames() {
-		return accountRepository.getAllUsernames();
-	}
-	
-	@Transactional(readOnly = true)
 	@PreAuthorize("hasRole('ROLE_ADMIN') or (#username == principal.username)")
-	public Account findOne(String username) {
-		return accountRepository.findOne(username);
-	}
+	Account findOne(String username) ;
 }
