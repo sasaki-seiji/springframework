86f0911e95203ad972f1d3c4dbaa1f8ce6b86ed2
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-11-25 08:40:35 +0900

    9.7.5: invalid-session-xml

---
+++ ch09/9-7/invalid-session-xml/src/main/java/example/app/AdminController.java
@@ -0,0 +1,20 @@
+package example.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+
+	@RequestMapping(path = "menu", method = RequestMethod.GET)
+	public String adminMenu() {
+		return "adminMenu";
+	}
+
+	@RequestMapping(path = "accounts/menu", method = RequestMethod.GET)
+	public String accountsMenu() {
+		return "accountsMenu";
+	}
+}
