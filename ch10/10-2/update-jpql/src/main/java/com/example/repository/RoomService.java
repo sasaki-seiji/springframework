9646c42c6856f1a6e1e2a095896b1793d1901fc3
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-12-17 14:16:40 +0900

    10.2.2: update-jpql

---
+++ ch10/10-2/update-jpql/src/main/java/com/example/repository/RoomService.java
@@ -0,0 +1,28 @@
+package com.example.repository;
+
+import java.util.List;
+
+import com.example.domain.model.Equipment;
+import com.example.domain.model.Room;
+
+public interface RoomService {
+	// 10.2.2: update JPQL
+	Integer updateCapacityAll(Integer capacity);
+	
+	// 10.2.2: JOIN FETCH
+	List<Room> getRoomsByNameFetch(String roomName);
+	
+	// 10.2.1: CRUD operations with JPA
+	Room getRoom(Integer roomId);
+	Room createRoom(String roomName, Integer capacity);
+	Room updateRoomName(Integer id, String roomName);
+	void deleteRoom(Integer id);
+	List<Room> getAllRooms();
+	
+	// 10.1.6: JPQL
+	List<Room> getRoomsByName(String roomName);
+	
+	// 10.1.5: relation
+	List<Equipment> getEquipmentsInRoom(Integer roomId);
+	Room getRoomOfEquipment(Integer equipmentId);
+}
