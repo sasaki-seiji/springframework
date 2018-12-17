e9243ba2b4c37e2cc356cc96b4dbb8eb02a4d186
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-12-17 11:55:59 +0900

    10.2.2: join-fetch

---
+++ ch10/10-2/join-fetch/src/main/java/com/example/join_fetch/App.java
@@ -0,0 +1,28 @@
+package com.example.join_fetch;
+
+import java.util.List;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.JpaConfig;
+import com.example.domain.model.Equipment;
+import com.example.domain.model.Room;
+import com.example.repository.RoomService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+       			= new AnnotationConfigApplicationContext(JpaConfig.class)) {
+       		RoomService roomService = context.getBean("roomService", RoomService.class);
+
+       		List<Room> rooms = roomService.getRoomsByNameFetch("セミナールーム");
+       		System.err.println("room(roomName=セミナールーム): " + rooms);
+
+       		List<Equipment> equipments = rooms.get(0).getEquipments();
+       		System.err.println("room(roomName=セミナールーム).equipments: " + equipments);
+    	}
+    }
+}
