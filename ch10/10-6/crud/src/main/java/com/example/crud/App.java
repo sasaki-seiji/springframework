fb371d92da703c48aa9bb20efae0afc1e2987bc7
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-12-22 11:40:49 +0900

    10.6.1: crud

---
+++ ch10/10-6/crud/src/main/java/com/example/crud/App.java
@@ -0,0 +1,39 @@
+package com.example.crud;
+
+import java.util.List;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.JpaConfig;
+import com.example.domain.model.Room;
+import com.example.domain.repository.RoomService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+       			= new AnnotationConfigApplicationContext(JpaConfig.class)) {
+       		RoomService roomService = context.getBean("roomService", RoomService.class);
+
+       		List<Room> rooms = roomService.getAllRooms();
+       		System.err.println("initially all rooms: " + rooms);
+       		
+       		Room room = roomService.getRoom(2);
+       		System.err.println("room(2): " + room);
+       		
+       		room = roomService.createRoom("new room", 50);
+       		System.err.println("room(new): " + room);
+      		
+       		room = roomService.updateRoomName(4, "new room name");
+       		System.err.println("room(4): " + room);
+
+       		roomService.deleteRoom(4);
+       		System.err.println("room(4): deleted");
+
+       		rooms = roomService.getAllRooms();
+       		System.err.println("finally all rooms: " + rooms);
+    	}
+    }
+}
