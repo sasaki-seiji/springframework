96a7975bad8b0bdea45d711682a7eea36a5d7d56
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-12-24 10:39:52 +0900

    update progress

--- ch10/sql/insert-init-data.sql
+++ ch10/sql/insert-init-data.sql
@@ -1,6 +1,22 @@
 INSERT INTO room(room_name, capacity) VALUES ('幹部用会議室', 10);
 INSERT INTO room(room_name, capacity) VALUES ('セミナールーム', 30);
 INSERT INTO room(room_name, capacity) VALUES ('カンファレンスルーム', 100);
+INSERT INTO room(room_name, capacity) VALUES ('room-101', 8);
+INSERT INTO room(room_name, capacity) VALUES ('room-102', 8);
+INSERT INTO room(room_name, capacity) VALUES ('room-103', 8);
+INSERT INTO room(room_name, capacity) VALUES ('room-104', 8);
+INSERT INTO room(room_name, capacity) VALUES ('room-105', 8);
+INSERT INTO room(room_name, capacity) VALUES ('room-106', 6);
+INSERT INTO room(room_name, capacity) VALUES ('room-107', 6);
+INSERT INTO room(room_name, capacity) VALUES ('room-108', 6);
+INSERT INTO room(room_name, capacity) VALUES ('room-201', 8);
+INSERT INTO room(room_name, capacity) VALUES ('room-202', 8);
+INSERT INTO room(room_name, capacity) VALUES ('room-203', 8);
+INSERT INTO room(room_name, capacity) VALUES ('room-204', 8);
+INSERT INTO room(room_name, capacity) VALUES ('room-205', 8);
+INSERT INTO room(room_name, capacity) VALUES ('room-206', 6);
+INSERT INTO room(room_name, capacity) VALUES ('room-207', 6);
+INSERT INTO room(room_name, capacity) VALUES ('room-208', 6);
 
 INSERT INTO equipment(room_id, equipment_name, equipment_count, equipment_remarks)
 	 VALUES (1, 'テレビ会議システム', 1, NULL);
