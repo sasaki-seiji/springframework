c4e4870f341ee6f595446c227ed584fcd057d446
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-12-28 20:50:44 +0900

    10.2.2: update-jpql-pg

---
+++ ch10/10-2/update-jpql-pg/src/main/java/com/example/update_jpql_pg/App.java
@@ -0,0 +1,30 @@
+package com.example.update_jpql_pg;
+
+import java.util.List;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.JpaConfig;
+import com.example.domain.model.Room;
+import com.example.domain.repository.RoomService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+       			= new AnnotationConfigApplicationContext(JpaConfig.class)) {
+       		RoomService roomService = context.getBean("roomService", RoomService.class);
+
+       		List<Room> rooms = roomService.getAllRooms();
+       		System.err.println("initial rooms state: " + rooms);
+
+       		int count = roomService.updateCapacityAll(50);
+       		System.err.println("updateCapaityAll(50): " + count);
+
+       		rooms = roomService.getAllRooms();
+       		System.err.println("final rooms state: " + rooms);
+    	}
+    }
+}
