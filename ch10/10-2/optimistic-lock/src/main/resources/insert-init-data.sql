995deba2c5b9faa23c04fe3e2f11c974fd0690af
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-12-29 03:23:57 +0900

    update progress

--- ch10/10-2/optimistic-lock/src/main/resources/insert-init-data.sql
+++ ch10/10-2/optimistic-lock/src/main/resources/insert-init-data.sql
@@ -1,6 +1,10 @@
-INSERT INTO room(room_name, capacity, version) VALUES ('幹部用会議室', 10, 1);
-INSERT INTO room(room_name, capacity, version) VALUES ('セミナールーム', 30, 1);
-INSERT INTO room(room_name, capacity, version) VALUES ('カンファレンスルーム', 100, 1);
+INSERT INTO room(room_name, capacity) VALUES ('幹部用会議室', 10);
+INSERT INTO room(room_name, capacity) VALUES ('セミナールーム', 30);
+INSERT INTO room(room_name, capacity) VALUES ('カンファレンスルーム', 100);
+
+INSERT INTO room_with_version(room_name, capacity, version) VALUES ('幹部用会議室', 10, 1);
+INSERT INTO room_with_version(room_name, capacity, version) VALUES ('セミナールーム', 30, 1);
+INSERT INTO room_with_version(room_name, capacity, version) VALUES ('カンファレンスルーム', 100, 1);
 
 INSERT INTO equipment(room_id, equipment_name, equipment_count, equipment_remarks)
 	 VALUES (1, 'テレビ会議システム', 1, NULL);
@@ -12,3 +16,5 @@ INSERT INTO equipment(room_id, equipment_name, equipment_count, equipment_remark
 	 VALUES (2, 'プロジェクタ', 5, '移動可能です');
 INSERT INTO equipment(room_id, equipment_name, equipment_count, equipment_remarks)
 	 VALUES (2, 'ホワイトボード', 6, '移動可能です');
+
+	 
--- ch10/10-2/optimistic-lock/src/main/resources/schema.sql
+++ ch10/10-2/optimistic-lock/src/main/resources/schema.sql
@@ -1,6 +1,12 @@
 CREATE TABLE room (
   room_id       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   room_name 	VARCHAR(30) NOT NULL,
+  capacity		INT NOT NULL
+);
+
+CREATE TABLE room_with_version (
+  room_id       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  room_name 	VARCHAR(30) NOT NULL,
   capacity		INT NOT NULL,
   version		INT NOT NULL
 );
@@ -11,5 +17,6 @@ CREATE TABLE equipment (
   equipment_name 	VARCHAR(30) NOT NULL,
   equipment_count	INT NOT NULL,
   equipment_remarks	VARCHAR(100),
-  FOREIGN KEY (room_id) REFERENCES room(room_id)
+  FOREIGN KEY (room_id) REFERENCES room_with_version(room_id)
 );
+
