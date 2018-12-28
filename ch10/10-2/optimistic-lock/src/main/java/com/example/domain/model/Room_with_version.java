8d9f10f3a9b327670b6f95b0d9c31583dd19e317
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-12-29 07:38:17 +0900

    10.2.3: optimistic-lock: resolve warning

--- ch10/10-2/optimistic-lock/src/main/java/com/example/domain/model/Room_with_version.java
+++ ch10/10-2/optimistic-lock/src/main/java/com/example/domain/model/Room_with_version.java
@@ -1,16 +1,12 @@
 package com.example.domain.model;
 
 import java.io.Serializable;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
