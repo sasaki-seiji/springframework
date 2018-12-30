4570c447be2331e81967b1dd8ad7b8d96897f261
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2018-12-30 16:37:54 +0900

    10.1.6: jpql-pg

---
+++ ch10/10-1/jpql-pg/src/main/java/com/example/jpql_pg/App.java
@@ -0,0 +1,25 @@
+package com.example.jpql_pg;
+
+import java.util.List;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.JpaConfig;
+import com.example.domain.model.Room;
+import com.example.domain.repository.RoomService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+       			= new AnnotationConfigApplicationContext(JpaConfig.class)) {
+       		RoomService roomService = context.getBean("roomService", RoomService.class);
+
+       		List<Room> rooms = roomService.getRoomsByName("セミナールーム");
+       		System.err.println("rooms(roomName='セミナールーム'): " + rooms);
+      		
+       	}
+    }
+}
