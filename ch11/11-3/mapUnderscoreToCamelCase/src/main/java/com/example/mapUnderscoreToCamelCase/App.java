58735e4ebcfa4159ff1b9d478ec1ec5f658e10a3
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-05 07:29:58 +0900

    11.3.5: mapUnderscoreToCamelCase

---
+++ ch11/11-3/mapUnderscoreToCamelCase/src/main/java/com/example/mapUnderscoreToCamelCase/App.java
@@ -0,0 +1,31 @@
+package com.example.mapUnderscoreToCamelCase;
+
+import java.util.List;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.AppConfig;
+import com.example.domain.model.MeetingRoom;
+import com.example.domain.repository.MeetingRoomService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+       			= new AnnotationConfigApplicationContext(AppConfig.class)) {
+        	MeetingRoomService meetingRoomService 
+        		= context.getBean("meetingRoomService", MeetingRoomService.class);
+
+        	MeetingRoom meetingRoom = meetingRoomService.findOne("002");
+        	System.out.println("findOne(002): " + meetingRoom);
+        	
+        	long count = meetingRoomService.count();
+        	System.out.println("count(): " + count);
+        	
+        	List<MeetingRoom> list = meetingRoomService.findAll();
+        	System.out.println("findAll(): " + list);
+    	}
+    }
+}
