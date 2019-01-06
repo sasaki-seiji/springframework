1caf19fc89ca32ab425ddad9cbca9dba08766aac
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-06 14:43:03 +0900

    11.3.6: SelectKey-annotation

---
+++ ch11/11-3/SelectKey-annotation/src/main/java/com/example/domain/mapper/MeetingRoomMapper.java
@@ -0,0 +1,33 @@
+package com.example.domain.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+
+import com.example.domain.model.MeetingRoom;
+
+public interface MeetingRoomMapper {
+
+	// 11.3.5: select
+
+	@Select("SELECT room_id AS roomId, room_name AS roomName, capacity"
+			+ " FROM meeting_room WHERE room_id = #{roomId}")
+	MeetingRoom findOne(String roomId);
+
+	@Select("SELECT COUNT(*) FROM meeting_room")
+	long count();
+
+	@Select("SELECT room_id AS roomId, room_name AS roomName, capacity"
+			+ " FROM meeting_room ORDER BY room_id")
+	List<MeetingRoom> findAll();
+	
+	// 11.3.6: insert
+	
+	@SelectKey(statement = "SELECT RANDOM_UUID()", keyProperty = "roomId", 
+			before = true, resultType = String.class)	
+	@Insert("INSERT INTO meeting_room (room_id, room_name, capacity)"
+			+ " VALUES (#{roomId}, #{roomName}, #{capacity})")
+	void create(MeetingRoom meetingRoom);
+}
