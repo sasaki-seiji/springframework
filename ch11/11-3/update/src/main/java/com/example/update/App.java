365f9b039be5d9b870ac06e4d639d5e84730cb82
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-07 19:48:44 +0900

    11.3.7: update

---
+++ ch11/11-3/update/src/main/java/com/example/update/App.java
@@ -0,0 +1,36 @@
+package com.example.update;
+
+import java.util.List;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.AppConfig;
+import com.example.domain.model.MeetingRoom;
+import com.example.domain.repository.MeetingRoomService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+       			= new AnnotationConfigApplicationContext(AppConfig.class)) {
+        	MeetingRoomService meetingRoomService 
+        		= context.getBean("meetingRoomService", MeetingRoomService.class);
+
+        	List<MeetingRoom> list = meetingRoomService.findAll();
+        	System.out.println("initially: " + list);
+
+        	MeetingRoom meetingRoom = new MeetingRoom();
+        	meetingRoom.setRoomId("002");
+        	meetingRoom.setRoomName("コンファレンスルーム");
+        	meetingRoom.setCapacity(50);
+        	
+        	boolean updated = meetingRoomService.update(meetingRoom);
+        	System.out.println("update(" + meetingRoom + ") returns " + updated);
+        	
+        	list = meetingRoomService.findAll();
+        	System.out.println("finally: " + list);
+    	}
+    }
+}
