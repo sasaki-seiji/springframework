960263a3427c77ac12750a78aafe3ea6ed39dae1
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-09 20:38:28 +0900

    11.3.8: Delete-annotation

---
+++ ch11/11-3/Delete-annotation/src/main/java/com/example/Delete_annotation/App.java
@@ -0,0 +1,32 @@
+package com.example.Delete_annotation;
+
+import java.util.List;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.AppConfig;
+import com.example.domain.model.MeetingRoom;
+import com.example.domain.repository.MeetingRoomService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+       			= new AnnotationConfigApplicationContext(AppConfig.class)) {
+        	MeetingRoomService meetingRoomService 
+        		= context.getBean("meetingRoomService", MeetingRoomService.class);
+
+        	List<MeetingRoom> list = meetingRoomService.findAll();
+        	System.out.println("initially: " + list);
+
+        	String id = "002";
+        	boolean deleted = meetingRoomService.delete(id);
+        	System.out.println("delete(" + id + ") returns " + deleted);
+        	
+        	list = meetingRoomService.findAll();
+        	System.out.println("finally: " + list);
+    	}
+    }
+}
---
+++ ch11/11-3/Delete-annotation/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO meeting_room VALUES ('001', '幹部用会議室', 10);
+INSERT INTO meeting_room VALUES ('002', '大会議室', 30);
+INSERT INTO meeting_room VALUES ('003', '会議室A', 15);
