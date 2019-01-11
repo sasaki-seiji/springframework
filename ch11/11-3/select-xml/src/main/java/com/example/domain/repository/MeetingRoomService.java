86a15607714f4e2e43374621a530f8369e9b668a
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-11 19:10:11 +0900

    10.3.5: select-xml

---
+++ ch11/11-3/select-xml/src/main/java/com/example/domain/repository/MeetingRoomService.java
@@ -0,0 +1,31 @@
+package com.example.domain.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.mapper.MeetingRoomMapper;
+import com.example.domain.model.MeetingRoom;
+
+@Transactional
+@Service
+public class MeetingRoomService {
+
+	@Autowired
+	MeetingRoomMapper meetingRoomMapper;
+	
+	public MeetingRoom findOne(String roomId) {
+		return meetingRoomMapper.findOne(roomId);
+	}
+	
+	public long count() {
+		return meetingRoomMapper.count();
+	}
+	
+	public List<MeetingRoom> findAll() {
+		return meetingRoomMapper.findAll();
+	}
+	
+}
