f1a2b5a623bbbf008b6aa00542d1519b3cd89611
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-12 06:47:42 +0900

    11.4.1: where-if: modify meeting room data

--- ch11/11-4/where-if/src/main/resources/data.sql
+++ ch11/11-4/where-if/src/main/resources/data.sql
@@ -6,5 +6,6 @@ INSERT INTO meeting_room VALUES ('A-001', 'MR-01', 10);
 INSERT INTO meeting_room VALUES ('A-002', 'MR-02', 20);
 INSERT INTO meeting_room VALUES ('A-003', 'MR-03', 30);
 
-INSERT INTO meeting_room VALUES ('B-001', 'CR-01', 50);
-INSERT INTO meeting_room VALUES ('B-002', 'CR-02', 100);
+INSERT INTO meeting_room VALUES ('B-001', 'CR-01', 80);
+INSERT INTO meeting_room VALUES ('B-002', 'CR-02', 150);
+INSERT INTO meeting_room VALUES ('B-003', 'CR-03', 200);
