6c7157d5c1f0b66345b16c5dff07f5304d683180
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-12 06:39:04 +0900

    11.4.1: where-if

---
+++ ch11/11-4/where-if/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO meeting_room VALUES ('001', '幹部用会議室', 10);
+INSERT INTO meeting_room VALUES ('002', '大会議室', 30);
+INSERT INTO meeting_room VALUES ('003', '会議室A', 15);
+
+INSERT INTO meeting_room VALUES ('A-001', 'MR-01', 10);
+INSERT INTO meeting_room VALUES ('A-002', 'MR-02', 20);
+INSERT INTO meeting_room VALUES ('A-003', 'MR-03', 30);
+
+INSERT INTO meeting_room VALUES ('B-001', 'CR-01', 50);
+INSERT INTO meeting_room VALUES ('B-002', 'CR-02', 100);
