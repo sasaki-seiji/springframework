3f3436fc8c8562389a74174a9273bc40675b7cc2
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-12 09:31:42 +0900

    11.4.1: where-foreach

---
+++ ch11/11-4/where-foreach/src/main/java/com/example/where_foreach/App.java
@@ -0,0 +1,31 @@
+package com.example.where_foreach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.AppConfig;
+import com.example.domain.model.MeetingRoom;
+import com.example.domain.repository.MeetingRoomService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+       			= new AnnotationConfigApplicationContext(AppConfig.class)) {
+        	MeetingRoomService meetingRoomService 
+        		= context.getBean("meetingRoomService", MeetingRoomService.class);
+
+        	List<String> roomIds = new ArrayList<String>();
+        	roomIds.add("001");
+        	roomIds.add("A-002");
+        	roomIds.add("B-003");
+        	
+        	List<MeetingRoom> meetingRooms = meetingRoomService.findByRoomIds(roomIds);
+        	System.out.println("findByRoomIds(001, A-002, B-003): " + meetingRooms);
+    	}
+    }
+}
---
+++ ch11/11-4/where-foreach/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO meeting_room VALUES ('001', '幹部用会議室', 10);
+INSERT INTO meeting_room VALUES ('002', '大会議室', 30);
+INSERT INTO meeting_room VALUES ('003', '会議室A', 15);
+
+INSERT INTO meeting_room VALUES ('A-001', 'MR-01', 10);
+INSERT INTO meeting_room VALUES ('A-002', 'MR-02', 20);
+INSERT INTO meeting_room VALUES ('A-003', 'MR-03', 30);
+
+INSERT INTO meeting_room VALUES ('B-001', 'CR-01', 80);
+INSERT INTO meeting_room VALUES ('B-002', 'CR-02', 150);
+INSERT INTO meeting_room VALUES ('B-003', 'CR-03', 200);
