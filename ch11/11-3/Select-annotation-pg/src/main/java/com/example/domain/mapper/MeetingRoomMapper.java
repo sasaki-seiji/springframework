cfabcb164ffa6b77df0af7bf7d09d7852a6b2ff3
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-14 14:54:19 +0900

    11.3.5: Select-annotation-pg

---
+++ ch11/11-3/Select-annotation-pg/src/main/java/com/example/domain/mapper/MeetingRoomMapper.java
@@ -0,0 +1,21 @@
+package com.example.domain.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.example.domain.model.MeetingRoom;
+
+public interface MeetingRoomMapper {
+	
+	@Select("SELECT room_id AS roomId, room_name AS roomName, capacity"
+			+ " FROM meeting_room WHERE room_id = #{roomId}")
+	MeetingRoom findOne(String roomId);
+
+	@Select("SELECT COUNT(*) FROM meeting_room")
+	long count();
+
+	@Select("SELECT room_id AS roomId, room_name AS roomName, capacity"
+			+ " FROM meeting_room ORDER BY room_id")
+	List<MeetingRoom> findAll();
+}
