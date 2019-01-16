53cf062ff47f52b283866c0091fd1e80e95a78c3
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-16 20:46:02 +0900

    11.3.6: insert-pg

---
+++ ch11/11-3/insert-pg/src/main/java/com/example/insert_pg/App.java
@@ -0,0 +1,33 @@
+package com.example.insert_pg;
+
+import java.util.List;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.AppConfig;
+import com.example.domain.model.MeetingRoom;
+import com.example.domain.repository.MeetingRoomService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+       			= new AnnotationConfigApplicationContext(AppConfig.class)) {
+        	MeetingRoomService meetingRoomService 
+        		= context.getBean("meetingRoomService", MeetingRoomService.class);
+
+        	MeetingRoom newMeetingRoom = new MeetingRoom();
+        	newMeetingRoom.setRoomId("004");
+        	newMeetingRoom.setRoomName("会議室B");
+        	newMeetingRoom.setCapacity(8);
+        	
+        	meetingRoomService.create(newMeetingRoom);
+        	System.out.println("created:");
+        	
+        	List<MeetingRoom> list = meetingRoomService.findAll();
+        	System.out.println("findAll(): " + list);
+    	}
+    }
+}
