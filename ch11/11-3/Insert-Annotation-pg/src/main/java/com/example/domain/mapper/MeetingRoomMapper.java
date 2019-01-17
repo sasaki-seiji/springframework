681a0831e0415c7c652338e992a875d7994ef240
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-17 19:08:10 +0900

    11.3.6: Insert-annotation-pg

---
+++ ch11/11-3/Insert-Annotation-pg/src/main/java/com/example/domain/mapper/MeetingRoomMapper.java
@@ -0,0 +1,30 @@
+package com.example.domain.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.domain.model.MeetingRoom;
+
+public interface MeetingRoomMapper {
+
+	// 11.3.5: select
+
+	@Select("SELECT room_id AS roomId, room_name AS roomName, capacity"
+			+ " FROM meeting_room WHERE room_id = #{roomId}")
+	MeetingRoom findOne(String roomId);
+
+	@Select("SELECT COUNT(*) FROM meeting_room")
+	long count();
+
+	@Select("SELECT room_id AS roomId, room_name AS roomName, capacity"
+			+ " FROM meeting_room ORDER BY room_id")
+	List<MeetingRoom> findAll();
+	
+	// 11.3.6: insert
+	
+	@Insert("INSERT INTO meeting_room (room_id, room_name, capacity)"
+			+ " VALUES (#{roomId}, #{roomName}, #{capacity})")
+	void create(MeetingRoom meetingRoom);
+}
