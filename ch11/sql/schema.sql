27cfa354a4f8bfed08706e6ac1088dcc31732eb8
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-24 21:09:38 +0900

    update progress

--- ch11/sql/schema.sql
+++ ch11/sql/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE meeting_room;
 
 CREATE TABLE meeting_room (
-  room_id       VARCHAR(10) NOT NULL PRIMARY KEY,
+  room_id       VARCHAR(36) NOT NULL PRIMARY KEY,
   room_name 	VARCHAR(30) NOT NULL,
   capacity		INT NOT NULL
 );
