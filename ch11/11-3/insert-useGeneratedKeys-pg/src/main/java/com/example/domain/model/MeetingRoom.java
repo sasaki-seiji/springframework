7ff9b15d500038edd24d51fb82a9bb1d9ce03301
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-26 06:55:34 +0900

    11.3.6: insert-useGeneratedKeys-pg

---
+++ ch11/11-3/insert-useGeneratedKeys-pg/src/main/java/com/example/domain/model/MeetingRoom.java
@@ -0,0 +1,22 @@
+package com.example.domain.model;
+
+public class MeetingRoom {
+	
+	// 2019.01.26 change: String -> Integer
+	private Integer roomId;
+	private String roomName;
+	private int capacity;
+	
+	public Integer getRoomId() { return this.roomId; }
+	public void setRoomId(Integer id) { this.roomId = id; }
+	public String getRoomName() { return this.roomName; }
+	public void setRoomName(String name) { this.roomName = name; }
+	public int getCapacity() { return this.capacity; }
+	public void setCapacity(int capacity) { this.capacity = capacity; }
+
+	public String toString() {
+		return "{ roomId: " + this.roomId
+				+ ", roomName: " + this.roomName
+				+ ", capacity: " + this.capacity + " }";
+	}
+}
