c6f339da257d2544f8f2c72fe1fda788cc8f8364
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-26 06:57:13 +0900

    update progress

--- ch11/sql/data.sql
+++ ch11/sql/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO meeting_room VALUES ('001', 'Š²•”—p‰ï‹cŽº', 10);
 INSERT INTO meeting_room VALUES ('002', '‘å‰ï‹cŽº', 30);
 INSERT INTO meeting_room VALUES ('003', '‰ï‹cŽºA', 15);
+
+INSERT INTO meeting_room_serial (room_name, capacity) VALUES ('Š²•”—p‰ï‹cŽº', 10);
+INSERT INTO meeting_room_serial (room_name, capacity) VALUES ('‘å‰ï‹cŽº', 30);
+INSERT INTO meeting_room_serial (room_name, capacity) VALUES ('‰ï‹cŽºA', 15);
--- ch11/sql/schema.sql
+++ ch11/sql/schema.sql
@@ -5,3 +5,12 @@ CREATE TABLE meeting_room (
   room_name 	VARCHAR(30) NOT NULL,
   capacity		INT NOT NULL
 );
+
+
+DROP TABLE meeting_room_serial;
+
+CREATE TABLE meeting_room_serial (
+  room_id       SERIAL PRIMARY KEY,
+  room_name 	VARCHAR(30) NOT NULL,
+  capacity		INT NOT NULL
+);
