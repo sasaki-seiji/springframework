67d0fb68e39c22fdc18fbff2c35ed1fa42743cf0
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-01-29 20:09:15 +0900

    11.4.1: where-if-pg

---
+++ ch11/11-4/where-if-pg/src/main/java/com/example/domain/mapper/MeetingRoomMapper.java
@@ -0,0 +1,25 @@
+package com.example.domain.mapper;
+
+import java.util.List;
+
+import com.example.domain.model.MeetingRoom;
+import com.example.domain.model.MeetingRoomCriteria;
+
+public interface MeetingRoomMapper {
+	// 11.3.5: select
+	MeetingRoom findOne(String roomId);
+	long count();
+	List<MeetingRoom> findAll();
+	
+	// 11.3.6: insert
+	void create(MeetingRoom meetingRoom);
+
+	// 11.3.7: update
+	boolean update(MeetingRoom meetingRoom);
+	
+	// 11.3.8: delete
+	boolean delete(String roomId);
+	
+	// 11.4.1: dynamic SQL building on mapping file
+	List<MeetingRoom> findByCriteria(MeetingRoomCriteria criteria);
+}
