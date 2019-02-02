6509f0c21acc42e08389b747c29948fd0902ae1d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-02 16:34:09 +0900

    update progress

--- ch11/sql/data.sql
+++ ch11/sql/data.sql
@@ -10,6 +10,12 @@ INSERT INTO meeting_room VALUES ('B-001', 'CR-01', 80);
 INSERT INTO meeting_room VALUES ('B-002', 'CR-02', 150);
 INSERT INTO meeting_room VALUES ('B-003', 'CR-03', 200);
 
+INSERT INTO reservable_room VALUES ('A-001', '2019-01-12');
+INSERT INTO reservable_room VALUES ('A-001', '2019-01-13');
+INSERT INTO reservable_room VALUES ('A-001', '2019-01-14');
+INSERT INTO reservable_room VALUES ('A-002', '2019-01-12');
+INSERT INTO reservable_room VALUES ('A-002', '2019-01-13');
+
 INSERT INTO meeting_room_serial (room_name, capacity) VALUES ('Š²•”—p‰ï‹cŽº', 10);
 INSERT INTO meeting_room_serial (room_name, capacity) VALUES ('‘å‰ï‹cŽº', 30);
 INSERT INTO meeting_room_serial (room_name, capacity) VALUES ('‰ï‹cŽºA', 15);
--- ch11/sql/schema.sql
+++ ch11/sql/schema.sql
@@ -6,6 +6,14 @@ CREATE TABLE meeting_room (
   capacity		INT NOT NULL
 );
 
+DROP TABLE reservable_room;
+
+CREATE TABLE reservable_room (
+	room_id			VARCHAR(36) NOT NULL,
+	reserved_date	DATE NOT NULL,
+	PRIMARY KEY (room_id, reserved_date),
+	FOREIGN KEY (room_id) REFERENCES meeting_room(room_id) 
+);
 
 DROP TABLE meeting_room_serial;
 
