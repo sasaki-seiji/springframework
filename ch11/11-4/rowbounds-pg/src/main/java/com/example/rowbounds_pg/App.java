ee40bf43f6febda41872c66b247bddd94aed6268
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-10 16:55:49 +0900

    11.4.4: rowbounds-pg

---
+++ ch11/11-4/rowbounds-pg/src/main/java/com/example/rowbounds_pg/App.java
@@ -0,0 +1,27 @@
+package com.example.rowbounds_pg;
+
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.AppConfig;
+import com.example.domain.model.MeetingRoom;
+import com.example.domain.repository.MeetingRoomService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try(ConfigurableApplicationContext context 
+       			= new AnnotationConfigApplicationContext(AppConfig.class)) {
+        	MeetingRoomService meetingRoomService 
+        		= context.getBean("meetingRoomService", MeetingRoomService.class);
+
+        	RowBounds rowBounds = new RowBounds(5, 5);
+        	List<MeetingRoom> meetingRooms = meetingRoomService.findAll(rowBounds);
+        	System.out.println("findAll(RowBounds(5,5)): " + meetingRooms);
+    	}
+    }
+}
