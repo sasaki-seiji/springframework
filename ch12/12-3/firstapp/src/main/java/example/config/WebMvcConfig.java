cc518653723813e8e5ff36abf69e35b9fd330fea
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-02-11 11:50:02 +0900

    12:3.1: firstapp: add top view

---
+++ ch12/12-3/firstapp/src/main/java/example/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("example.app")
+@Import(ThymeleafConfig.class)
+public class WebMvcConfig implements WebMvcConfigurer {
+
+}
