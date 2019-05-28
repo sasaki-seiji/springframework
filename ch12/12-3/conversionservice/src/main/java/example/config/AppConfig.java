93fe6cf9c8e4b5a513f1a4d10934e4e34dd17b02
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-05-29 08:11:55 +0900

    12.3.8: conversionservice: add PropertySource

--- ch12/12-3/conversionservice/src/main/java/example/config/AppConfig.java
+++ ch12/12-3/conversionservice/src/main/java/example/config/AppConfig.java
@@ -1,8 +1,11 @@
 package example.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+// 2019.05.29 add: see 2.4.1
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 
 }
