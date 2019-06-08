b3e387bdbcd30b36ea16b59824522fce60449e6f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-06-08 09:04:29 +0900

    13.2.1: restful

---
+++ ch13/13-2/restful/src/main/java/com/example/MessagesController.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("messages")
+public class MessagesController {
+	final List<Message> messages = new CopyOnWriteArrayList<>();
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Message> getMessages() {
+		return messages;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public Message postMessage(@RequestBody Message message){
+		messages.add(message);
+		return message;
+	}
+}
