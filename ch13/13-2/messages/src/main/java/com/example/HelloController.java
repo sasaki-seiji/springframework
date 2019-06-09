39f8f00c1ec913c772b39717127219ed165f3726
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-06-09 19:56:42 +0900

    13.2.2: messages

---
+++ ch13/13-2/messages/src/main/java/com/example/HelloController.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@Autowired
+	MessageSource messageSource;
+	
+	@RequestMapping("/hello")
+	public String hello(Model model, Locale locale) {
+		model.addAttribute("title", 
+				messageSource.getMessage("app.title", null, locale));
+		model.addAttribute("hello", "Hello World!");
+		return "hello";
+	}
+}
