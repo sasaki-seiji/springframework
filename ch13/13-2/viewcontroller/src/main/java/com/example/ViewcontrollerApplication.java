aff0e00a5f26f4dd393ce604064ecf8217c56c3f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-06-09 20:19:47 +0900

    13.2.2: viewcontroller

---
+++ ch13/13-2/viewcontroller/src/main/java/com/example/ViewcontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewcontrollerApplication.class, args);
+	}
+
+}
