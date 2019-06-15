dd3849a435dd531cd6c8e833bcbfa54b3fa4ae61
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-06-15 13:00:23 +0900

    13.3.2: DataJpaH2

---
+++ ch13/13-3/DataJpaH2/src/main/java/com/example/DataJpaH2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaH2Application.class, args);
+	}
+
+}
