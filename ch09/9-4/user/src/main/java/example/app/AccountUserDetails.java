3d2fe9b24b6b671a856c317ccbd1265d88886280
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-06-22 05:55:13 +0900

    9.4.3: user: @SuppressWarnings for NoOpPasswordEncoder

--- ch09/9-4/user/src/main/java/example/app/AccountUserDetails.java
+++ ch09/9-4/user/src/main/java/example/app/AccountUserDetails.java
@@ -10,6 +10,7 @@ public class AccountUserDetails extends User {
 	private static final long serialVersionUID = 1L;
 	private final Account account;
 	
+	// see https://docs.spring.io/spring-security/site/docs/4.2.12.RELEASE/apidocs/org/springframework/security/core/userdetails/User.html
 	public AccountUserDetails(
 			Account account, Collection<GrantedAuthority> authorities) {
 		super(account.getUsername(), account.getPassword(),
