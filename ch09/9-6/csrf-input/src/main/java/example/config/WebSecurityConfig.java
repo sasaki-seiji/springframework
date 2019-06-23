b9bc8a4d4eb0bde931c881725f53532fc9ffd6e7
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-06-23 16:15:51 +0900

    9.6.2: csrf-input: @SuppressWarnings for NoOpPasswordEncoder

--- ch09/9-6/csrf-input/src/main/java/example/config/WebSecurityConfig.java
+++ ch09/9-6/csrf-input/src/main/java/example/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@SuppressWarnings("deprecation") // for NoOpPasswordEncoder
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -44,7 +45,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	// 2018.11.11 add: needed for spring security 5
 	// see https://www.harinathk.com/spring/no-passwordencoder-mapped-id-null/
-	@SuppressWarnings("deprecation")
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance(); // temporary use
