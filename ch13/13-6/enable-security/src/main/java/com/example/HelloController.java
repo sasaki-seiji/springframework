a30f2922998622211f4f6d518b288fb032de9701
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-07-07 12:01:20 +0900

    13.6.4: enable-security: display username into welcome page

--- ch13/13-6/enable-security/src/main/java/com/example/HelloController.java
+++ ch13/13-6/enable-security/src/main/java/com/example/HelloController.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
+	// 2019.07.07 change: see 9.4.11
 	@RequestMapping("/")
-	public String welcome() {
+	public String welcome(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+		String user = userDetails.getUsername();
+		model.addAttribute("user", user);
 		return "welcome";
 	}
 	
