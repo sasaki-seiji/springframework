1390c7cfbcfdb9ab9ba988a10d528c8db6d368d8
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2019-07-07 10:25:46 +0900

    13.4.2: custom-auth: display username in welcome page

--- ch13/13-4/custom-auth/src/main/java/com/example/HelloController.java
+++ ch13/13-4/custom-auth/src/main/java/com/example/HelloController.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
+	// 2019.07.07 change: see 9.4.11
 	@RequestMapping("/")
-	public String welcome() {
+	public String welcome(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+		String user = userDetails.getUsername();
+		model.addAttribute("user", user);
 		return "welcome";
 	}
 	
 	@RequestMapping("/hello")
 	public String hello(Model model) {
-		model.addAttribute("hello", "Hello Thymeleaf!");
+		model.addAttribute("hello", "Hello customized authentication demo!");
 		return "hello";
 	}
 }
